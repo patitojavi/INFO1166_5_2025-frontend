f9a8be44af2e94bc947cf13495216d4959da3074
patitojavi <patojavier1981@gmail.com>
2025-09-10 21:21:39 -0300

    se quitaron paginas home y experience porque no se usarán

--- app/src/pages/Experience.tsx
+++
@@ -1,8 +0,0 @@
-export default function Experience() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">experiencia</h1>
-      <p className="mt-2 text-gray-700">si</p>
-    </div>
-  );
-}
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import BNE from "../pages/BNE";
-import Home from "../pages/Home";
-import Experience from "../pages/Experience";
+
 import Login from "../pages/Login";
 import Register from "../pages/Register.tsx";
 import Protected from "./Protected";
@@ -9,10 +8,8 @@ import Dashboard from "../pages/Dashboard.tsx";
 
 export const router = createBrowserRouter([
   { path: "/", element: <BNE /> },                
-  { path: "/home", element: <Home /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },   
-  { path: "/experiencia", element: <Protected><Experience /></Protected> },
   { path: "/perfil", element: <Protected><Dashboard /></Protected> },
   
   
