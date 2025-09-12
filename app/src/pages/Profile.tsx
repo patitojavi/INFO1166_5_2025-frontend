fb2b764bbd76e2fb9b9bf09a8c72bc48261c885d
patitojavi <patojavier1981@gmail.com>
2025-09-11 22:22:21 -0300

    borre la pagina perfil, (no se usa)

--- app/src/pages/Profile.tsx
+++
@@ -1,8 +0,0 @@
-export default function Profile() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Mi Perfil</h1>
-      <p className="mt-2 text-gray-700">hola</p>
-    </div>
-  );
-}
